c4160db5e986da4cbec800f8b7595563a508870b
Kevin Amado <kamadorueda@gmail.com>
2019-02-16 18:49:58 -0500

    reduced complexity

--- datapipeline.py
+++ datapipeline.py
@@ -4,7 +4,6 @@ import io
 import os
 import sys
 import json
-import argparse
 import urllib.request
 from time import sleep
 from time import time as curr_timestamp
@@ -12,42 +11,58 @@ from time import time as curr_timestamp
 if not os.path.isdir("data"):
     os.makedirs("data")
 
+
 def json_as_string(obj):
     return json.dumps(obj, indent=4)
+
 
 def print_stderr(message):
     print(message, file=sys.stderr, flush=True)
 
+
 def print_stdout(message):
     print(message, file=sys.stdout, flush=True)
 
+
+def stop_pipeline(state, exit_code):
+    print_stdout(json_as_string(state))
+    print_stderr(json_as_string(state))
+    exit(exit_code)
+
+
+def write_to_file(match_id, data_source, data):
+    with open(f"data/{match_id}.{data_source}.json", "w") as response:
+        response.write(data)
+
+
 class API_Worker():
     def __init__(self, region, api_token):
-        self.min_sslr = 1.2 # min seconds since last request
-        self.lrts = curr_timestamp() # last request timestamp
-        self.headers = {"X-Riot-Token": api_token}
+        # min seconds to wait since last request
+        self.wait = 1.2
+        # last request timestamp
+        self.last = curr_timestamp()
+        # api url and headers
         self.api_url = f"https://{region}.api.riotgames.com"
-    def request(self, resource):
-        # wait needed time to not exceed the rate limit
-        sleep(max(
-            - curr_timestamp()
-            + self.lrts
-            + self.min_sslr, 0.0))
-        self.lrts = curr_timestamp()
+        self.api_headers = {"X-Riot-Token": api_token}
 
-        response, status = None, None
+    def request(self, resource):
+        # wait needed time not to exceed the rate limit
+        sleep(max(
+            + self.last
+            + self.wait
+            - curr_timestamp(), 0.0))
+        self.last = curr_timestamp()
+
         try:
-            request = urllib.request.Request(resource, headers=self.headers)
-            response = urllib.request.urlopen(request).read().decode('utf-8')
-            status = 200
+            request = urllib.request.Request(
+                resource, headers=self.api_headers)
+            connection = urllib.request.urlopen(request)
+            status, response = 200, connection.read().decode('utf-8')
         except urllib.error.HTTPError as error:
-            status = error.code
-            if status == 429:
-                print_stderr(f"INFO: [WORKER] [SLEEP]")
-                sleep(self.min_sslr)
-                return request(resource)
+            status, response = error.code, None
         except urllib.error.URLError:
-            pass
+            status, response = None, None
+
         return status, response
 
     def lol_match_v4_matches(self, match_id):
@@ -66,60 +81,34 @@ def main():
     print_stderr(json_as_string(state))
 
     match_id = state["match_id"]
-    game_timestamp = state["game_timestamp"]
     worker = API_Worker(state["region"], state["api_token"])
 
-    if game_timestamp > curr_timestamp() - 86400.0:
-        print_stdout(json_as_string(state))
+    # fetch data for 60 seconds
+    limit_timestamp = curr_timestamp() + 60.0
 
-    limit_timestamp = curr_timestamp() + 1.0*10.0
+    # api endpoints
+    api_endpoints = (
+        ("match_info", worker.lol_match_v4_matches),
+        ("match_timeline", worker.lol_match_v4_timelines),
+    )
 
     while curr_timestamp() < limit_timestamp:
-        status_match, response_match = \
-            worker.lol_match_v4_matches(match_id)
-
-        if status_match == 200:
-            print_stderr(f"INFO: [MATCH] [{match_id}]")
-            with open(f"data/{match_id}.matchinfo.json", "w") as response:
-                response.write(response_match)
-            game_timestamp = json.loads(response_match)["gameCreation"] / 1000.0
-        elif status_match == 404:
-            print_stderr(f"INFO: [MATCH] [{match_id}] [{status_match}] [Data not found]")
-            match_id += 1
-            continue
-        elif status_match in [401, 403]:
-            print_stderr("INFO: Unauthorized/Forbidden")
-            print_stdout(json_as_string(state))
-            exit(1)
-        else:
-            print_stderr(f"INFO: [MATCH] [{match_id}] [{status_match}]")
-            continue
-
-        status_timeline, response_timeline = \
-            worker.lol_match_v4_timelines(match_id)
-
-        if status_timeline == 200:
-            print_stderr(f"INFO: [TIMEL] [{match_id}]")
-            with open(f"data/{match_id}.timeline.json", "w") as response:
-                response.write(response_timeline)
-        elif status_timeline in [401, 403]:
-            print_stderr("INFO: Unauthorized/Forbidden")
-            print_stdout(json_as_string(state))
-            exit(1)
-        else:
-            print_stderr(f"INFO: [TIMEL] [{match_id}] [{status_match}]")
-            continue
+        for endpoint_name, endpoint_wrapper in api_endpoints:
+            status, response = endpoint_wrapper(match_id)
+            print_stderr(f"INFO: [{endpoint_name}] [{match_id}] [{status}]")
+            if status == 200:
+                write_to_file(match_id, endpoint_name, response)
+            elif not status == 404:
+                stop_pipeline(state, exit_code=1)
 
         match_id += 1
 
-    new_state = {
+    # write the new state
+    stop_pipeline({
         "match_id": match_id,
-        "game_timestamp": game_timestamp,
         "region": state["region"],
         "api_token": state["api_token"]
-    }
-    print_stdout(json_as_string(new_state))
-    print_stderr(json_as_string(new_state))
+    }, exit_code=0)
 
 
 if __name__ == '__main__':
